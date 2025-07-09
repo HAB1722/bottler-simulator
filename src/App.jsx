cc2e36eb82f311f508639351365095579804fe8b
HAB1722 <ahmadbdr99@gmail.com>
2025-07-09 18:04:42 +0300

    Added App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FactoryDashboard from './components/FactoryDashboard';
+
+const App = () => (
+  <div className="p-4">
+    <h1 className="text-2xl font-bold mb-4">Water Bottling Factory Simulator</h1>
+    <FactoryDashboard />
+  </div>
+);
+
+export default App;
